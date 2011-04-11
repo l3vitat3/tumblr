cbd0aa90968ba8e514309204af91a9f2c79c9867
l3vitat3 <just.l3vitat3@googlemail.com>
2011-04-11 23:28:53 +0200

    added starting quote

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('started');
+console.log(
+	'"Only two things are infinite,\n' 
+	+'the universe and human stupidity,\n'
+	+'and I\'m not sure about the former."\n\n'
+	+'Albert Einstein\n'
+	+'US (German-born) physicist (1879 - 1955)'
+);
+
+
